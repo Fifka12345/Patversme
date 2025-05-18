12f3973eafff5c7005ea2bc12881b24471584435
FiveJurgis <161245331+Fifka12345@users.noreply.github.com>
2025-05-18 18:03:33 +0300

    Update writtenWithCS.cs

--- writtenWithCS.cs
+++ writtenWithCS.cs
@@ -4,23 +4,31 @@ public class Shelter
 {
     public static void Main()
     {
+        //data
+		string shelterName;
+		int dogs;
+		int cats;
+		int animals;
+		int dogPercent;
+		int catPercent;
+		
         bool tryAgain = true;
         while (tryAgain)
         {
             try
             {
                 Console.WriteLine("Please, write shelter name: ");
-                string shelterName = Console.ReadLine();
+                shelterName = Console.ReadLine();
 
                 Console.WriteLine("Please, enter amount of dogs: ");
-                int dogs = Convert.ToInt32(Console.ReadLine());
+                dogs = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Please, enter amount of cats: ");
-                int cats = Convert.ToInt32(Console.ReadLine());
+                cats = Convert.ToInt32(Console.ReadLine());
 
-                int animals = cats + dogs;
-                int dogPercent = (int)Math.Round((double)(100 * dogs) / animals);
-                int catPercent = (int)Math.Round((double)(100 * cats) / animals);
+                animals = cats + dogs;
+                dogPercent = (int)Math.Round((double)(100 * dogs) / animals);
+                catPercent = (int)Math.Round((double)(100 * cats) / animals);
 
                 Console.WriteLine($"Shelter: {shelterName} takes care of {animals} animals. Dogs make {dogPercent}% in the shelter, but cats make {catPercent}% in the shelter.");
                 tryAgain = false;
